3daffb1a3a75eb213451efce667e1a363901e2b9
Yoni <139280075+yoni825@users.noreply.github.com>
2024-03-12 17:32:15 -0400

    Add files via upload

---
+++ tekl8388_l01/src/t04.py
@@ -0,0 +1,27 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Yonathan Tekle
+ID:      169068388
+Email:  tekl8388@mylaurier.ca
+__updated__ = "2024-03-12"
+-------------------------------------------------------
+"""
+name = input("Please enter your name: ")
+print("Pleased to meet you ")
+print(name)
+
+
+def func():
+    """
+    -------------------------------------------------------
+    description
+    Use: 
+    -------------------------------------------------------
+    Parameters:
+        name - description (type)
+    Returns:
+        name - description (type)
+    ------------------------------------------------------
+    """
---
+++ tekl8388_l01/src/t06.py
@@ -0,0 +1,29 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Yonathan Tekle
+ID:      169068388
+Email:  tekl8388@mylaurier.ca
+__updated__ = "2024-03-12"
+-------------------------------------------------------
+"""
+# Calculate total pay
+salary = 2500.0
+bonus = 1200.0
+pay = salary + bonus
+print("Your pay is", pay)
+
+
+def func():
+    """
+    -------------------------------------------------------
+    description
+    Use: 
+    -------------------------------------------------------
+    Parameters:
+        name - description (type)
+    Returns:
+        name - description (type)
+    ------------------------------------------------------
+    """
